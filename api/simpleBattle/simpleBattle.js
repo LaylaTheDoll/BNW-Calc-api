e09aa41c3eb7b93eeba1535af71af005d90fe3a3
TwelveZimmer <dlggames2@gmail.com>
2020-04-28 17:51:06 -0300

    Update simpleBattle.js

--- api/simpleBattle/simpleBattle.js
+++ api/simpleBattle/simpleBattle.js
@@ -9,8 +9,8 @@
 let simulate = function(body){ //função principal
     return new Promise((resolve, reject) => {
         console.log(JSON.stringify(body));
-        const atkRoll = d20();
-        const defRoll = d20();
+        const atkRoll = 1;
+        const defRoll = 1;
         
         let isAtkOk = checkAtk(body, atkRoll, defRoll);
         let dmg = finalAtk(body, atkRoll, defRoll);
@@ -25,9 +25,20 @@ let simulate = function(body){ //função principal
     }
 
     let checkAtk = function(body, atkRoll, defRoll){ //função para verificação de acerto
-        let check = (body.attacker.precision + atkRoll)  - (body.defenders[0].ND + defRoll) ;
-        return (check >= 0);
+        let isCheckOK = checkAtkCritFail(body, atkRoll, defRoll);
+        return (isCheckOK >= 1);
     }
+
+    let checkAtkCritFail = function(body, atkRoll, defRoll){ // função para confirmação de falha crítica
+        let isCritFailOk = critFail(body, atkRoll);
+        let atkCheck = (body.attacker.precision + atkRoll)  - (body.defenders[0].ND + defRoll);
+        isCritFailOk = isCritFailOk ?  0 : atkCheck;
+        return (isCritFailOk);
+    }
+        let critFail = function(body, atkRoll){ // função para verificar se a rolagem é igual ou menor a taxa de falha crítica
+            let checkFail = atkRoll;
+            return (checkFail <= body.attacker.critFailureRate);
+        }
 
     let finalAtk = function(body,atkRoll){ //funcão para verificação de dano
         let atk = critDMG(body, atkRoll);
